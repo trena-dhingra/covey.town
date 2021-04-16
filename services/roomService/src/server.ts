623b2eb110a1d372b81e17e69ec96c0cb75a07e2
mdhanapal1812 <muthouazhagi1812@gmail.com>
2021-04-16 19:46:34 -0400

    server changes

--- services/roomService/src/server.ts
+++ services/roomService/src/server.ts
@@ -3,7 +3,8 @@ import CORS from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import io from 'socket.io';
 import * as jwt from 'jsonwebtoken';
-import jwksRsa from 'jwks-rsa';
+import jwksRsa, { CertSigningKey, SigningKey } from 'jwks-rsa';
+import { AnyCnameRecord } from 'dns';
 import {
   townSubscriptionHandler,
 } from './requestHandlers/CoveyTownRequestHandlers';
@@ -15,13 +16,18 @@ const app = Express();
 app.use(Express.json());
 app.use(CORS());
 
+export type KeyType = (
+  publicKey: CertSigningKey,
+  rsaPublicKey: SigningKey,
+) => void;
+
 // Authentication
 const client = jwksRsa({
   jwksUri: 'https://dev-fse.us.auth0.com/.well-known/jwks.json',
 });
 
-function getKey(header: any, cb: any) : any{
-  client.getSigningKey(header.kid, (_:any, key: any) : any=> {
+function getKey(header: any, cb: any) : void {
+  client.getSigningKey(header.kid, (_:Error|null, key: any) : void => {
     const signingKey = key.publicKey || key.rsaPublicKey;
     cb(null, signingKey);
   });
@@ -39,16 +45,15 @@ const options = {
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) :any => {
+  context: ({ req }) :{ user: Promise<unknown>; } => {
     // simple auth check on every request
     const token = req.headers.authorization || '';
-    const user = new Promise((resolve, reject) : any=> {
-      jwt.verify(token, getKey, options, (err : any, decoded: any):any => {
+    const user = new Promise((resolve, reject) : void => {
+      jwt.verify(token, getKey, options, (err : Error|null, decoded:any): void => {
         if (err) {
           return reject(err);
         }
         return resolve((decoded as any).email);
-
       });
     });
 
